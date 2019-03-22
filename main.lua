bab8b6eb2a4cb91081c0d473368d3e530bcb4ca4
Bijan <bijanbwb@gmail.com>
2019-03-21 22:14:45 -0400

    Music, text, and graphics

--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
 function love.load()
   whale = love.graphics.newImage("whale.png")
+  sound = love.audio.newSource("music.ogg", "stream")
+  love.audio.play(sound)
 end
 
 function love.draw()
+  love.graphics.print("Whaley", 300, 180)
   love.graphics.draw(whale, 300, 200)
 end
