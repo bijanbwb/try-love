4904e762ec4cc0ade7aee26b6b846a1dcdf2bef3
Bijan <bijanbwb@gmail.com>
2019-03-23 13:38:00 -0400

    Initial pong setup

--- main.lua
+++ main.lua
@@ -13,17 +13,88 @@ function love.load()
   }
 
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {})
+
+  -- Give the font a retro appearance
+  love.graphics.setDefaultFilter("nearest", "nearest")
+
+  -- Font
+  font = love.graphics.newFont("fonts/font.ttf", 160)
+  love.graphics.setFont(font)
+end
+
+-- UPDATE
+
+function love.keypressed(key)
+  if key == "escape" then
+    love.event.quit("restart")
+  end
 end
 
 -- DRAW
 
 function love.draw()
-  x_offset = 40
-  y_offset = 6
+  horizontal_center = WINDOW_WIDTH / 2
+  player_one_score = 0
+  player_two_score = 0
+  vertical_center = WINDOW_HEIGHT / 2
 
-  horizontal_center = WINDOW_WIDTH / 2 - x_offset
-  vertical_center = WINDOW_HEIGHT / 2 - y_offset
+  -- Player 1 Score
+  love.graphics.print(tostring(player_one_score), 280, 40)
 
-  -- Draw text in the center of the window
-  love.graphics.print('Hello Pong!', horizontal_center, vertical_center)
+  -- Player 2 Score
+  love.graphics.print(tostring(player_two_score), 880, 40)
+
+  -- Player 1 Paddle
+  playerOnePaddle = {
+    height = 125,
+    width = 25,
+    x = 75,
+    y = vertical_center - 75
+  }
+
+  love.graphics.rectangle(
+    "fill",
+    playerOnePaddle.x,
+    playerOnePaddle.y,
+    playerOnePaddle.width,
+    playerOnePaddle.height
+  )
+
+    -- Player 2 Paddle
+    playerTwoPaddle = {
+      height = 125,
+      width = 25,
+      x = WINDOW_WIDTH - 100,
+      y = vertical_center - 75
+    }
+
+    love.graphics.rectangle(
+      "fill",
+      playerTwoPaddle.x,
+      playerTwoPaddle.y,
+      playerTwoPaddle.width,
+      playerTwoPaddle.height
+    )
+
+    -- Ball
+    ball = {
+      height = 25,
+      width = 25,
+      x = horizontal_center - 12,
+      y = vertical_center - 12
+    }
+
+    love.graphics.rectangle(
+      "fill",
+      ball.x,
+      ball.y,
+      ball.width,
+      ball.height
+    )
+end
+
+-- QUIT
+
+function love.quit()
+  print("Thanks for playing!")
 end
